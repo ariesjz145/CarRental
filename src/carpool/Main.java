429b508a8334d9b273c4465be185b3e6c51d0c4d
Josh <ariesjz145@live.com>
2018-10-22 09:58:15 -0400

    Has CreateNewUser screen. Added fxml file from SceneBuilder

--- src/carpool/Main.java
+++ src/carpool/Main.java
@@ -8,34 +8,34 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-  private static Stage primaryStage;
+    private static Stage primaryStage;
 
-  @Override
-  public void start(Stage primaryStage) throws Exception{
+    @Override
+    public void start(Stage primaryStage) throws Exception{
 
-    setPrimaryStage(primaryStage);
+        setPrimaryStage(primaryStage);
 
-    Parent root = FXMLLoader.load(getClass().getResource("LoginMain.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("LoginMain.fxml"));
 
-    primaryStage.setTitle("Super Awesome Car Pool GUI Made By Five Good Looking Dudes");
-    primaryStage.setScene(new Scene(root, 1080, 400));
-    root.getStylesheets().add(getClass().getResource("styleSheet.css")
-        .toExternalForm());
+        primaryStage.setTitle("Super Awesome Car Pool GUI Made By Five Good Looking Dudes");
+        primaryStage.setScene(new Scene(root, 1080, 400));
+        root.getStylesheets().add(getClass().getResource("styleSheet.css")
+            .toExternalForm());
 
-    primaryStage.show();
-  }
-  private void setPrimaryStage(Stage stage) {
+        primaryStage.show();
+    }
+    private void setPrimaryStage(Stage stage) {
 
-    Main.primaryStage = stage;
-  }
+        Main.primaryStage = stage;
+    }
 
-  static public Stage getPrimaryStage() {
+    static public Stage getPrimaryStage() {
 
-    return primaryStage;
-  }
+        return primaryStage;
+    }
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    launch(args);
-  }
+        launch(args);
+    }
 }
